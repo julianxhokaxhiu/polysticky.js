7147ddb0c6d495aac450aed65c6ba39f3ee7af33
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2018-01-19 10:43:46 +0100

    0.0.4

--- dist/polysticky.js
+++ dist/polysticky.js
@@ -115,7 +115,7 @@ var PolySticky = function () {
       var me = this;
 
       if (me.browserNeedsPolyfill()) {
-        var el = me.fetchElement(selector);
+        var el = typeof selector === 'string' ? me.fetchElement(selector) : [selector];
 
         if (el.length) me.polyfill(el[0], window.getComputedStyle(el[0]));
       }
